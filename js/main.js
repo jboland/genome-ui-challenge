6b3df806d78639a8b4d9b593515b1850bc63afd7
Joe Boland <josephjboland@gmail.com>
2015-03-27 19:57:47 -0400

    Add eventlistener for nav links to toggle active class

--- js/main.js
+++ js/main.js
@@ -6,6 +6,21 @@ var bindEvents = function() {
 		document.getElementById('nav-list').classList.toggle('show');
 	});
 
+	var nav_links = document.querySelectorAll('nav ul li');
+
+	for (var i = 0; i < nav_links.length; i++) {
+		nav_links[i].addEventListener('click', function(event) { 
+			var other_active = document.querySelector('.active');
+			if (other_active && other_active != this) {
+				other_active.classList.toggle('active');
+			}
+			this.classList.toggle('active');
+			if (window.innerWidth < 600) {
+				document.getElementById('nav-list').classList.toggle('show');
+			}
+		});
+	}
+
 }
 
 window.onload = function() {
