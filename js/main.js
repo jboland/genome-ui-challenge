612f480dde93d8f295b4d31ae7350e91637bb1d6
Joe Boland <josephjboland@gmail.com>
2015-03-27 09:19:34 -0400

    Event listener to open menu on mobile

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-smoothScroll.init();
+var bindEvents = function() {
+	smoothScroll.init();
+
+	document.querySelector('.mobile-menu-toggle').addEventListener('click', function(event) {
+		event.preventDefault();
+		document.getElementById('nav-list').classList.toggle('show');
+	});
+
+}
+
+window.onload = function() {
+	bindEvents();
+}
